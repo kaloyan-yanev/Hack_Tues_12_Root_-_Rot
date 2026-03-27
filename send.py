87d32c4808d8abc8e9ce8d133a98a122ba8b5780
Bobisan <boris.strinsky@protonmail.com>
2026-03-27 12:21:21 +0200

    Wrote scripts for sending stir signal and temperature threshold value

--- send.py
+++
@@ -1,5 +0,0 @@
-import sys
-import paho.mqtt.client as mqtt
-
-broker = "localhost"
-topic = "esp32/ledfreq"
---
+++ send_stir.py
@@ -0,0 +1,21 @@
+import sys
+import paho.mqtt.client as mqtt
+import argparse
+
+parser = argparse.ArgumentParser(
+        prog='Send_Tresh',
+        description='Sends treshold update to esp')
+parser.add_argument('device')
+parser.add_argument('v', type=bool)
+args = parser.parse_args()
+
+device = args.device
+value = args.v
+
+topic = "machines/control/stir/" + device
+broker = "localhost"
+
+client = mqtt.Client()
+client.connect(broker, 1883)
+client.publish(topic, value)
+
