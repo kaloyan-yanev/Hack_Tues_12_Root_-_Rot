7c7d187604c11da2efb5bb4d902db9ad5736780d
Georgi15 <georgi.r.iliev.2022@elsys-bg.org>
2026-03-28 01:56:41 +0200

    have the same problem as 12 hours ago but forgot how to fix it we now run the file with dotnet run

--- RootAndRot.Server/obj/Debug/net10.0/RootAndRot.Server.AssemblyInfo.cs
+++ RootAndRot.Server/obj/Debug/net10.0/RootAndRot.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RootAndRot.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29a3711160f53e10499c504d665e27a0a6045d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9323b9e81319b4208afdaec3073e48720738b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("RootAndRot.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RootAndRot.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
